0914817ae44d0be3c918753cd789ba71c5da2d99
Deepanshu Kumar <anshudeep077@outlook.com>
2023-08-03 16:17:49 +0530

    first commit

--- index.js
+++ index.js
@@ -12,75 +12,52 @@ const day = d.getDay();
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
 
 const m = new Date();
-const month = m.getMonth() + 1;
+const month = m.getMonth();
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const y = new Date();
 const year = y.getFullYear();
 
+const da = new Date();
+const date = da.getDate();
+
+if (day != day + 1) {
+    var items = [];
+    var items2 = [];
+}
+else {
+    var items = [];
+    var items2 = [];
+}
+
 app.get("/", (req, res) => {
-
-    res.render("index.ejs",{task: items, Weekday: dayNames[day], Month: monthNames[month],Day: day, Year: year});
-
+    res.render("index.ejs", { task: items, Weekday: dayNames[day], Month: monthNames[month], Day: date, Year: year });
 });
+
 app.get("/today", (req, res) => {
 
-    res.render("index.ejs",{task: items, Weekday: dayNames[day], Month: monthNames[month],Day: day, Year:year});
+    res.render("index.ejs", { task: items, Weekday: dayNames[day], Month: monthNames[month], Day: date, Year: year });
 
 });
 
-app.get("/work", (req, res)=>
-{
-    res.render("work.ejs", {task2: items2, Year: year});
+app.get("/work", (req, res) => {
+    res.render("work.ejs", { task2: items2, Year: year });
 })
 
-if(day != day + 1)
-{
-    var items = [];
-    var items2 = [];
-}
-else{
-    var items = [];
-    var items2 = [];
-}
 
 app.post("/submit", (req, res) => {
-
-    if(req.body["task"] === "")
-    {
-       
-        res.render("index.ejs",{task: items, Weekday: dayNames[day], Month: monthNames[month],Day: day, Year: year})
-        
-    }
-    else
-    {
-        var item = req.body["task"];
-        items.push(item);
-        
-        res.render("index.ejs", {task: items, Weekday: dayNames[day], Month: monthNames[month],Day: day, Year: year});
-    }
-
+    var item = req.body["task"];
+    items.push(item);
+    const url = req.url;
+    res.redirect('/today');
 });
 
 app.post("/submit2", (req, res) => {
-
-    if(req.body["task2"] === "")
-    {
-       
-        res.render("work.ejs",{task2: items2, Year: year})
-        
-    }
-    else
-    {
-        var item2 = req.body["task2"];
-        items2.push(item2);
-        
-        res.render("work.ejs", {task2: items2, Year: year});
-    }
-
+    var item2 = req.body["task2"];
+    items2.push(item2);
+    const url = req.url;
+    res.redirect('/work');
 });
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
